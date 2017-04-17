379da499d84debb80ec7f9b70ac6925454a0fde7
Louis-Denomme <louis@louis-Aspire-E5-575>
2017-04-17 11:27:37 +0200

    Essai pour intérompre l'IA grâce aux threads (non concluants pour le moment)

--- src/projet/Jeu2048/AI.java
+++ src/projet/Jeu2048/AI.java
@@ -10,9 +10,29 @@ package projet.Jeu2048;
  * @author louis
  */
 public abstract class AI {
+    private static boolean interuption= true;
     
-    public static void tester(Fenetre2048 mainApp){
-        System.out.println("projet.Jeu2048.AI.tester()");
+    public static void resolveTheGame(Fenetre2048 mainApp, Thread t) throws InterruptedException{
+        //tant que la partie n'est pas finie ou que l'IA n'est pas mise sur pause
+        while(!AI.interuption||!mainApp.getMaGrille().partieFinie()){
+            System.out.println("Test du while");
+            t.sleep(1000);
+        }
+        
     }
     
+    public static String resolveOneTurn(Fenetre2048 mainApp){
+        String direction=null;
+        
+        return direction;
+    }
+    
+    public static void setInteruption(boolean interuption){
+        AI.interuption=interuption;
+    }
+    
+    public static boolean getInteruption(){
+        return AI.interuption;
+    }
 }
+
